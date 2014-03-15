dcc79f1d3be99108bea6d66fb2aac5ef80e8af65
Cory Giles <mail@corygil.es>
2014-03-15 02:57:58 -0500

    Update requirements and tests with hardcoded paths.

--- test/text/parse.py
+++ test/text/parse.py
@@ -1,7 +1,7 @@
 from BioTK.io import Treebank
 from BioTK.text import PCFG
 
-def test_pcfg():
-    treebank_path = "/home/gilesc/Data/GENIA/genia-treebank/division/dev.trees"
-    trees = Treebank.parse(treebank_path)
-    grammar = PCFG.build(trees[:5])
+#def test_pcfg():
+#    treebank_path = "/home/gilesc/Data/GENIA/genia-treebank/division/dev.trees"
+#    trees = Treebank.parse(treebank_path)
+#    grammar = PCFG.build(trees[:5])
